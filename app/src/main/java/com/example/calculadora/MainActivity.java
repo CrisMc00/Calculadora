28267eae0840b9cc845c541183f4aec5e08c3f52
CrisMc00 <cristianmc1609@gmail.com>
2024-10-11 21:42:51 -0600

    Initial commit

---
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textResultado;
+    private String entradaActual = "";
+    private String operador = "";
+    private double primerValor = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textResultado = findViewById(R.id.textView);
+
+    }
+}
